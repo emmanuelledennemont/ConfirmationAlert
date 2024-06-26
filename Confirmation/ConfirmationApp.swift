27a9db885fd813c1fee9088d24cdf8ca0542e625
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-26 11:31:34 +0400

    Initial Commit

---
+++ Confirmation/ConfirmationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfirmationApp.swift
+//  Confirmation
+//
+//  Created by Emmanuelle  Dennemont on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConfirmationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
